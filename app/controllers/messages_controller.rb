87569fa3e493452a6aaa368b569343a63a543308
gaotianminzu <71334065+gaotianminzu@users.noreply.github.com>
2020-09-27 23:24:27 +0900

    メッセージの表示_修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,9 +10,9 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-        redirect_to room_messages_path(@room)
+      redirect_to room_messages_path(@room)
     else
-      @messages =@room.messages.includes(:user)
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
